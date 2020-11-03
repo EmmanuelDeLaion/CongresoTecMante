3dd0fa4b0ba5d431b51a9cb6257c967cb05960b8
Emmanuel De Leon <deleon.lerma.17163@itsmante.edu.mx>
2020-11-02 22:35:27 -0600

    footer, contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { FaqsComponent } from './components/faqs/faqs.component';
 import { CabeceraComponent } from './components/cabecera/cabecera.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import * as AOS from 'aos';
 @NgModule({
   declarations: [
     AppComponent,
     FaqsComponent,
     CabeceraComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
